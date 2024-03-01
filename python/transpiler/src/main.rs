1d9ea85fb4403f9dcfff9eb284adeb3ef4182f94
charlie-sans <gamerboy030607@gmail.com>
2024-03-01 03:02:50 +0000

    Revert "now they kinda do, fixed the fact that the input was spliting new lines ans such."
    This reverts commit 1bc81533a53456067d165da00eea9e6f2f16b1e9.

--- python/transpiler/src/main.rs
+++ python/transpiler/src/main.rs
@@ -25,12 +25,18 @@ fn main() {
         // read an input file
         let input = std::fs::read("src/testcase.cusp").unwrap();
         // strip the newlines including any whitespace and tabs
-        let input = String::from_utf8_lossy(&input);
+        let input = String::from_utf8_lossy(&input).replace("\n", "").replace("\t", "").replace(" ", "");
         // tokenize the input
         println!("{}", input);
         lexer.lex(&input);
         println!("{:?}", lexer.tokens);
-        Parser::new(lexer.tokens).parse();
+        let parser = cubescript::Parser::new(lexer.tokens).parse(); // Wrap lexer.tokens in a Vec
+        println!("\n {:?}", parser);
+        
+        let transpiled_code = cubescript::transpile(Node::Program(parser));
+
+        
+        println!("\n {:?}", transpiled_code);
 
     };
 
